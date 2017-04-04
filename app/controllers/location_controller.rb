e4d6bece393432ec0f70affb414066b6df70b6c7
Elliot Patterson <sarah.elliot.rhodes@gmail.com>
2017-04-04 14:41:43 +0000

    Zip branch

---
+++ app/controllers/location_controller.rb
@@ -0,0 +1,4 @@
+class LocationController < ApplicationController
+  def zip:string
+  end
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,5 @@ class PagesController < ApplicationController
     def home
         @weather_lookup = WeatherLookup.new
     end
+    
 end
